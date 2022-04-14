fd5caff84af8931620a989e966bcc02a2fac4c27
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2022-04-14 14:25:52 +0300

    Add project files.

---
+++ Project.MAP/Options/AppUserMap.cs
@@ -0,0 +1,22 @@
+﻿using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.MAP.Options
+{
+    public class AppUserMap:BaseMap<AppUser>
+    {
+
+        public AppUserMap()
+        {
+            ToTable("Kullanıcılar");
+            Property(x => x.UserName).HasColumnName("Kullanıcı Ismi").IsRequired();
+            Property(x => x.Password).HasColumnName("Sifre").IsRequired().HasMaxLength(8);
+
+            HasOptional(x => x.Profile).WithRequired(x => x.AppUser); //Birebir ilişkinin tamamlanması zorunludur...
+        }
+    }
+}
---
+++ Project.MAP/Options/BaseMap.cs
@@ -0,0 +1,23 @@
+﻿using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.MAP.Options
+{
+    public abstract class BaseMap<T> : EntityTypeConfiguration<T> where T:BaseEntity
+    {
+
+        //C#'ta referans tipler siz müdahale etmediginizde Sql'e otomatik olarak null gecilebilir gider, deger tipler ise siz müdahale etmediginizde otomatik olarak null gecilemez gider...Bir deger tipinin SQL tarafında null gecilebilmesini istiyorsanız ilgili tipin nullable olarak işaretlenmesini saglamalısınız...
+        public BaseMap()
+        {
+            Property(x => x.CreatedDate).HasColumnName("Yaratılma Tarihi").HasColumnType("datetime2");
+            Property(x => x.ModifiedDate).HasColumnName("Guncelleme Tarihi").HasColumnType("datetime2");
+            Property(x => x.DeletedDate).HasColumnName("Silme Tarihi").HasColumnType("datetime2");
+            Property(x => x.Status).HasColumnName("Veri Durumu");
+        }
+    }
+}
